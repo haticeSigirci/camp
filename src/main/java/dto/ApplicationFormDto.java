27fe652f57aeb58046d8e645f2d51ccb6be669e3
haticeSigirci <hatice.sigirci@hotmail.com>
2015-08-18 12:12:52 +0300

    transforming application form with thymeleaf

--- src/main/java/dto/ApplicationFormDto.java
+++ src/main/java/dto/ApplicationFormDto.java
@@ -1,5 +1,8 @@
 package dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tr.org.lkd.lyk2015.camp.model.Application;
 import tr.org.lkd.lyk2015.camp.model.Student;
 
@@ -7,6 +10,8 @@ public class ApplicationFormDto {
 
 	private Application application = new Application();
 	private Student student = new Student();
+	
+	private List<Long> preferredCourseIds = new ArrayList<>();
 
 	public Student getStudent() {
 		return student;
@@ -22,6 +27,14 @@ public class ApplicationFormDto {
 
 	public void setApplication(Application application) {
 		this.application = application;
+	}
+
+	public List<Long> getPreferredCourseIds() {
+		return preferredCourseIds;
+	}
+
+	public void setPreferredCourseIds(List<Long> preferredCourseIds) {
+		this.preferredCourseIds = preferredCourseIds;
 	}
 
 }
